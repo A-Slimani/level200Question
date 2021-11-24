37e2f98ee22803718260f05beab5590fd1ffdace
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2021-11-25 10:31:12 +1100

    Add files via upload

--- level200Question.js
+++ level200Question.js
@@ -1,6 +1,6 @@
 // TODO: Modify this function
 function generateShortCode(storeId, transactionId) {
-      let sId = storeId.toString(36);
+  let sId = storeId.toString(36);
   let tId = transactionId.toString(36);
 
   let d = new Date();
@@ -12,13 +12,12 @@ function generateShortCode(storeId, transactionId) {
   if (sId.length < 2) sId = '0' + sId;
   while (tId.length < 3) tId = '0' + tId;
 
-
   return sId + tId + date + month + year;
 }
 
 // TODO: Modify this function
 function decodeShortCode(shortCode) {
-    let code = shortCode;
+  let code = shortCode;
 
   let pDate = new Date();
   let date = parseInt(code[5], 36);
@@ -41,31 +40,40 @@ function decodeShortCode(shortCode) {
 // --------------- Don't touch this area, all tests have to pass --------------- //
 // ------------------------------------------------------------------------------//
 function RunTests() {
+  var storeIds = [175, 42, 0, 9];
+  var transactionIds = [9675, 23, 123, 7];
 
-    var storeIds = [175, 42, 0, 9]
-    var transactionIds = [9675, 23, 123, 7]
-
-    storeIds.forEach(function (storeId) {
-        transactionIds.forEach(function (transactionId) {
-            var shortCode = generateShortCode(storeId, transactionId);
-            var decodeResult = decodeShortCode(shortCode);
-            $("#test-results").append("<div>" + storeId + " - " + transactionId + ": " + shortCode + "</div>");
-            AddTestResult("Length <= 9", shortCode.length <= 9);
-            AddTestResult("Is String", (typeof shortCode === 'string'));
-            AddTestResult("Is Today", IsToday(decodeResult.shopDate));
-            AddTestResult("StoreId", storeId === decodeResult.storeId);
-            AddTestResult("TransId", transactionId === decodeResult.transactionId);
-        })
-    })
+  storeIds.forEach(function (storeId) {
+    transactionIds.forEach(function (transactionId) {
+      var shortCode = generateShortCode(storeId, transactionId);
+      var decodeResult = decodeShortCode(shortCode);
+      $('#test-results').append(
+        '<div>' + storeId + ' - ' + transactionId + ': ' + shortCode + '</div>'
+      );
+      AddTestResult('Length <= 9', shortCode.length <= 9);
+      AddTestResult('Is String', typeof shortCode === 'string');
+      AddTestResult('Is Today', IsToday(decodeResult.shopDate));
+      AddTestResult('StoreId', storeId === decodeResult.storeId);
+      AddTestResult('TransId', transactionId === decodeResult.transactionId);
+    });
+  });
 }
 
 function IsToday(inputDate) {
-    // Get today's date
-    var todaysDate = new Date();
-    // call setHours to take the time out of the comparison
-    return (inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0));
+  // Get today's date
+  var todaysDate = new Date();
+  // call setHours to take the time out of the comparison
+  return inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0);
 }
 
 function AddTestResult(testName, testResult) {
-    var div = $("#test-results").append("<div class='" + (testResult ? "pass" : "fail") + "'><span class='tname'>- " + testName + "</span><span class='tresult'>" + testResult + "</span></div>");
+  var div = $('#test-results').append(
+    "<div class='" +
+      (testResult ? 'pass' : 'fail') +
+      "'><span class='tname'>- " +
+      testName +
+      "</span><span class='tresult'>" +
+      testResult +
+      '</span></div>'
+  );
 }
